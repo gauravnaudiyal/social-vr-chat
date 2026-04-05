2833bf4628e7cc269efbc8078e919cefe05749d5
Ignacy <ignacysus@gmail.com>
2026-04-05 13:06:19 +0100

    Altered logic for slide next/back button, now properly start and ends, with more optimized logic

--- social_vr/Assets/Scenes/LectureHall/SlidePresenter.cs
+++ social_vr/Assets/Scenes/LectureHall/SlidePresenter.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 public class SlidePresenter : MonoBehaviour
 {
     public Texture2D[] slides;   // drag your PNGs here in Inspector
-    int current = 0;
+    int current = -1;
 
     Renderer rend;
 
     void Start() => rend = GetComponent<Renderer>();
 
-    public void NextSlide()
-    {
-        current = (current + 1) % slides.Length;
-        rend.material.mainTexture = slides[current];
-    }
+  public void NextSlide()
+{
+    current = Mathf.Clamp(current + 1, 0, slides.Length - 1);
+    rend.material.mainTexture = slides[current];
+}
 
-    public void PrevSlide()
-    {
-        current = (current - 1 + slides.Length) % slides.Length;
-        rend.material.mainTexture = slides[current];
-    }
+public void PrevSlide()
+{
+    current = Mathf.Clamp(current - 1, 0, slides.Length - 1);
+    rend.material.mainTexture = slides[current];
+}
 }
